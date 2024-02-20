12356e70c29c27435384b3aad6afa5a586ffe185
Rui Augusto <rui.augusto.drumond@gmail.com>
2024-02-20 18:09:07 -0300

    chore: update displayed message after HRA submission

--- src/components/finish/finish_view.jsx
+++ src/components/finish/finish_view.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './../../assets/galileu_azul.png';
-import "./index.css"
+import React from "react";
+import logo from "./../../assets/galileu_azul.png";
+import "./index.css";
 
 const ThankYouPage = () => {
   return (
     <div className="thank-you-page">
-      <img src={logo} alt="Logo" className="logo" style={{ marginBottom: "100px" }} />
+      <img
+        src={logo}
+        alt="Logo"
+        className="logo"
+        style={{ marginBottom: "100px" }}
+      />
       <h2>Obrigado por preencher o formulário!</h2>
-      <p>Vamos avaliar as suas informações e logo logo você receberá uma mensagem com o resultado!</p>
+      <p>
+        Vamos avaliar as suas informações e logo logo você será incluído em uma
+        das nossas trilhas de saúde.
+      </p>
     </div>
   );
 };
--- src/components/finishSaude/finish_saude_view.jsx
+++ src/components/finishSaude/finish_saude_view.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './../../assets/saude.png';
-import "./index.css"
+import React from "react";
+import logo from "./../../assets/saude.png";
+import "./index.css";
 
 const ThankYouPageSaude = () => {
   return (
     <div className="thank-you-page2">
-      <img src={logo} alt="Logo" className="logo2" style={{ marginBottom: "100px" }} />
+      <img
+        src={logo}
+        alt="Logo"
+        className="logo2"
+        style={{ marginBottom: "100px" }}
+      />
       <h2>Obrigado por preencher o formulário!</h2>
-      <p>Vamos avaliar as suas informações e logo logo você receberá uma mensagem com o resultado!</p>
+      <p>
+        Vamos avaliar as suas informações e logo logo você será incluído em uma
+        das nossas trilhas de saúde.
+      </p>
     </div>
   );
 };
